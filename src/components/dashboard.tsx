3ba890f77e471f5f34888e61ad9e4b3ab2ab59f7
kumneger0 <kumnegerwondimu01@gmail.com>
2024-07-11 18:45:40 +0300

    Refactor: Make dashboard sidebar sticky

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -44,9 +44,9 @@ export function Dashboard({
   user: User;
 }) {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
+    <div className="grid min-h-screen relative w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="hidden sticky top-0 overflow-y-hidden border-r bg-muted/40 md:block">
+        <div className="flex  h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
               <CloudIcon className="h-6 w-6" />
@@ -273,7 +273,7 @@ export function Dashboard({
             <UserButton />
           </div>
         </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <main className="flex md:max-h-svh md:overflow-y-auto flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
           {children}
         </main>
       </div>
