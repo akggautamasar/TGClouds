06afa81dc650b2cf505c7683e14025d44aeeb8b6
akggautamasar <Worksbeyondworks@gmail.com>
2025-05-29 04:24:50 +0530

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,10 +3,12 @@ import * as schema from './schema';
 import { env } from '@/env';
 import { Pool } from 'pg';
 
+// Initialize PostgreSQL client pool using connection string from environment variables
 const client = new Pool({
-	connectionString: env.DATABASE_URL
+  connectionString: env.DATABASE_URL,
 });
 
+// Initialize Drizzle ORM instance with the PostgreSQL client and your schema
 export const db = drizzle(client, {
-	schema
+  schema,
 });
